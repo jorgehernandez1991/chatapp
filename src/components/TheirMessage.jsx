ac48c15c38c5ac9ba1cbae390464ed0607025874
Jorge Hernandez <jorgehernandez4791@gmail.com>
2021-04-01 16:11:06 -0700

    3rd commit

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -1,9 +1,34 @@
-const TheirMessage = () =>{
+const TheirMessage = ({ lastMessage ,message}) =>{
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username;
+
+
     return(
-        <div>
-            TheirMessage
+        <div className = 'message-row'>
+            {isFirstMessageByUser && (
+                <div
+                classname ="message-avatar"
+                style= {{backgroundImage: `url(${message?.sender?.avatar})`}}
+                />
+            )}
+            {
+                  message?.attachments?.lenght > 0
+                   ? (
+                        <img
+                        src = {message.attachments[0].file}
+                        alt ="message-attachment"
+                        className="message-image"
+                        style = {{ marginLeft:isFirstMessageByUser ? '4px' : '48px' }}
+                        />
+                    ) : (
+                        <div className= "message" style= {{float: 'left', marginRight : '18px', color: 'white', backgroundColor: '#3B2A50' }}>
+                        {message.text} 
+                         </div>
+                    )
+                }
+            
+        
         </div>
-    )
+    );
 
 }
 
